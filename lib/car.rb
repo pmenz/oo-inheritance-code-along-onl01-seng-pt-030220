3eff8bf17374ec5197ffdafab5b490192cd08985
Pablo Menz <p.menz.r@gmail.com>
2020-04-14 02:06:53 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,14 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+  
+ def initialize(wheel_size, wheel_number)
+   @wheel_size = wheel_size
+   @wheel_number = wheel_number
+end
+def go()
+  return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+end
+
+
+end
